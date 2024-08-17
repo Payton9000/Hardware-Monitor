449aeae6c308d3f75484af8ec0caa9c307a41d17
Payton9000 <121389791+Payton9000@users.noreply.github.com>
2024-08-17 17:14:35 +0800

    Update software version in AssemblyInfo.cs

--- CPUwenduhuoqu/Properties/AssemblyInfo.cs
+++ CPUwenduhuoqu/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("CPUwenduhuoqu")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Hardware Monitor")]
+[assembly: AssemblyDescription("Monitor hardware temperture, transmit it to ESP32")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Home")]
-[assembly: AssemblyProduct("CPUwenduhuoqu")]
+[assembly: AssemblyProduct("Hardware Monitor")]
 [assembly: AssemblyCopyright("Copyright © Home 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
